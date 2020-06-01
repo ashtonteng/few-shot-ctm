9ae20bc1c482beffa5d99ce0ae46d86f825bafa2
Hell Walker <ashtonteng@gmail.com>
2020-05-31 19:59:49 -0700

    gpu

--- core/config.py
+++ core/config.py
@@ -103,7 +103,7 @@ class Config(object):
 
     # ignore the resume file if set True
     ctrl.train_from_scratch = False
-    ctrl.device = 'cpu'
+    ctrl.device = 'gpu'
     ctrl.multi_gpu = False
     # how many iteration we want for each epoch and each test evaluation
     ctrl.total_iter_train = -1
@@ -245,7 +245,7 @@ class Config(object):
         self.logger('gpu_ids: {}\n'.format(self.ctrl.gpu_id))
         self.ctrl.multi_gpu = multi_gpu
         # for demo purpose
-        self.ctrl.device = 'cpu'
+        # self.ctrl.device = 'cpu'
         # self.ctrl.device = 'cuda'
 
     def _sanity_check(self):
